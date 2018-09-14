208585bfce8f2b5f9d529a694b152c0d381bcd23
Józef Bongowski <j.bongowski@protonmail.com>
2018-09-14 15:04:08 +0200

    Display bookmarks and delete each of them
    The commit adds the following functionality:
    - displaying the bookmarks as a colletion of items (links) each containg
    link button and remove button
    - possibility to remove single bookmarks
    - improvement of the feature which clear all bookmarks
    - reloading of the element displaying bookmarks after each change in the
    array containg bookmarks
    - small validation improvements

--- js/main.js
+++ js/main.js
@@ -37,25 +37,95 @@ function saveBookmark(event) {
 
     // set bookmarks back to local storage
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
   }
 
+  // clear the form fields after the sending
+  document.getElementById('bookmark-form').reset();
 
   // required to work properly in Chrome
   // prevent form from submiting
   event.preventDefault();
+
+  // reload element displaying bookmarks
+  displayBookmarks();
 }
 
 
-function clearBookmark() {
-  // test if local storage is empty
-  if (!(localStorage.getItem('bookmarks') === null)) {
-    // there is sth in local storage
+function clearBookmarks() {
+  // retrive bookmarks from the local storage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 
-    // delete all bookmarks
-    localStorage.removeItem('bookmarks');
+  // test if local storage is empty
+  if (Array.isArray(bookmarks) && bookmarks.length) {
+    // there is sth in local storage
+    // ask user to confirm action
+     if (window.confirm("Do you really want to delete all bookmarks?")) {
+       // delete all bookmarks
+       localStorage.removeItem('bookmarks');
+     }
+  } else {
+    window.alert('There\'s nothing to delete');
   }
-  // probably required to work properly in Chrome - not tested
-  event.preventDefault();
+
+  // reload element displaying bookmarks
+  displayBookmarks();
+}
+
+
+function deleteBookmark(name) {
+  // retrive bookmarks from the local storage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  // loop through the bookmarks looking for the bookmark with desirable name to delete
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].name === name) {
+      // bookmark is found
+
+      // delete the bookmark form the bookmarks array
+      bookmarks.splice(i, 1);
+      break;
+    }
+  }
+  // set bookmarks back to local storage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // reload element displaying bookmarks
+  displayBookmarks();
+}
+
+
+function displayBookmarks() {
+  // retrive bookmarks from the local storage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  // element in which the bookmarks will be displayed
+  var bookmarksResults = document.getElementById('bookmarks-results');
+
+  bookmarksResults.innerHTML = '';
+
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="collection-item center">' +
+    '<a class="deep-purple-text text-darken-3" href=' +
+    url +
+    ' target="_blank">' +
+    '<h5>' +
+    name +
+    '</h5>' +
+    '</a>' +
+    '<a class="btn-small waves-effect waves-light deep-purple darken-3" href=' +
+    url +
+    ' target="_blank"><i class="material-icons small">open_in_new</i></a>' +
+    '</a>' +
+    '<a id="clear-bookmarks" class="btn-small waves-effect waves-light red" onclick="deleteBookmark(\'' +
+    name +
+    '\')"><i class="material-icons small">delete</i></a>' +
+    '</div>'
+    ;
+  }
 }
 
 
@@ -64,4 +134,4 @@ function clearBookmark() {
 // because it will turn it immediately after the page is load
 document.getElementById('bookmark-form').addEventListener('submit', saveBookmark);
 
-document.getElementById('clear-bookmarks').addEventListener('click', clearBookmark);
+document.getElementById('clear-bookmarks').addEventListener('click', clearBookmarks);
